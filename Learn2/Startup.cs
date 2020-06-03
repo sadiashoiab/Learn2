782fda6f8240d265a6dcab8be9962b0dd4541fdb
Sadia Shoiab <47217909+sadiashoiab@users.noreply.github.com>
2020-06-03 16:20:18 -0500

    showing off zunair

--- Learn2/Startup.cs
+++ Learn2/Startup.cs
@@ -20,7 +20,7 @@ namespace Learn2
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the dsaia container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
